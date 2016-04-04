51017f7f3af45b6bd82d9cbb252d7737945fa94f
ATBennett <atbennett1222@gmail.com>
2016-04-04 20:35:52 +0100

    Added seperate gem classes

---
+++ include/BasicGems.h
@@ -0,0 +1,75 @@
+#include "Gem.h"
+#ifndef BASICGEMS_H
+#define BASICGEMS_H
+
+class RedGem: public Gem
+{
+    public:
+        RedGem();
+        virtual ~RedGem();
+        void printGem(int,int);
+        void printShrink1(int,int);
+        void printShrink2(int,int);
+};
+
+class GreenGem: public Gem
+{
+    public:
+        GreenGem();
+        virtual ~GreenGem();
+        void printGem(int,int);
+        void printShrink1(int,int);
+        void printShrink2(int,int);    
+};
+
+class YellowGem: public Gem
+{
+    public:
+        YellowGem();
+        virtual ~YellowGem();
+        void printGem(int,int);
+        void printShrink1(int,int);
+        void printShrink2(int,int);    
+};
+
+class BlueGem: public Gem
+{
+    public:
+        BlueGem();
+        virtual ~BlueGem();
+        void printGem(int,int);
+        void printShrink1(int,int);
+        void printShrink2(int,int);   
+};
+
+class MagentaGem: public Gem
+{
+    public:
+        MagentaGem();
+        virtual ~MagentaGem();
+        void printGem(int,int);
+        void printShrink1(int,int);
+        void printShrink2(int,int);    
+};
+
+class CyanGem: public Gem
+{
+    public:
+        CyanGem();
+        virtual ~CyanGem();
+        void printGem(int,int);
+        void printShrink1(int,int);
+        void printShrink2(int,int);    
+};
+
+class WhiteGem: public Gem
+{
+    public:
+        WhiteGem();
+        virtual ~WhiteGem();
+        void printGem(int,int);
+        void printShrink1(int,int);
+        void printShrink2(int,int);    
+};
+
+#endif // BASICGEMS_H
