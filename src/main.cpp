c35a114ec53a5ebe8b9c54293a117e0d614ba62b
ATBennett <atbennett1222@gmail.com>
2016-04-06 20:04:52 +0100

    Updated Comments

--- src/main.cpp
+++ src/main.cpp
@@ -6,28 +6,30 @@ using namespace std;
 
 int main()
 {
+    //ncurses initilisation
     initscr();
     noecho();
     cbreak();
     keypad(stdscr,TRUE);
-
     if(has_colors() == FALSE)
 	{	endwin();
 		printf("Your terminal does not support color\n");
 		return(1);
 	}
-
 	start_color();
 	curs_set(0);
-    Board MainBoard(8,8);
+
+    Board MainBoard(8,8);   //Creates an 8x8 board
     MainBoard.setTurns(30);
     MainBoard.printBoard();
-    vector<pair<int,int> > MatchedGems = MainBoard.matched();
-    while(!MatchedGems.empty())
+    vector<pair<int,int> > MatchedGems = MainBoard.matched(); //checks for any already matched gems
+    while(!MatchedGems.empty()) //Keeps going until there are no more matches
     {
         MainBoard.quickRemoveMatched(MatchedGems);
         MatchedGems = MainBoard.matched();
     }
+
+    //Main Gameplay loop
     while(MainBoard.getTurns()>0){
         clear();
         MainBoard.printBoard();
@@ -37,7 +39,7 @@ int main()
         else if(temp == KEY_DOWN) MainBoard.mvCursorV(1);
         else if(temp == KEY_LEFT) MainBoard.mvCursorH(-1);
         else if(temp == KEY_RIGHT) MainBoard.mvCursorH(1);
-        else if(temp == ' '){
+        else if(temp == ' '){ //If space is pressed enters gem swapping mode
             clear();
             MainBoard.setHighlight(true);
             MainBoard.printBoard();
@@ -50,7 +52,7 @@ int main()
                 case KEY_DOWN : dir = 'D'; break;
                 case KEY_LEFT : dir = 'L'; break;
                 case KEY_RIGHT : dir = 'R'; break;
-                default : dir = 'X'; break;
+                default : dir = 'X'; break; //if an invalid key is pressed set dir to 'X' to avoid breaking the swapGems function
             }
             MainBoard.setHighlight(false);
             if(MainBoard.swapGems(MainBoard.getCursor(),dir)){
@@ -59,11 +61,11 @@ int main()
                 refresh();
                 if(!MainBoard.matched().empty()){
                     MainBoard.setTurns(MainBoard.getTurns() - 1);
-                    while(!MainBoard.matched().empty()){
+                    while(!MainBoard.matched().empty()){ //Loops until there are no more matches
                         MainBoard.fancyRemoveMatched(MainBoard.matched());
                     }
                 }
-                else MainBoard.swapGems(MainBoard.getCursor(),dir);
+                else MainBoard.swapGems(MainBoard.getCursor(),dir); //If there are no matches, swap the gems back 
             }
         }
     }
