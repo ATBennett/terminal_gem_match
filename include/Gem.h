5f916ed17b8d8100696cbe6b9e88cfb436931fbd
ATBennett <atbennett1222@gmail.com>
2016-04-06 20:04:24 +0100

    Updated Comments

--- include/Gem.h
+++ include/Gem.h
@@ -1,3 +1,5 @@
+//Gem superclass 
+
 #ifndef GEM_H
 #define GEM_H
 
@@ -6,27 +8,27 @@ class Gem
 {
     private:
         bool falling;
-        bool newGem;
+        bool newGem;    //set to true on creation
     public:
         Gem();
         virtual ~Gem();
 
         void printVoid(int,int);
-        virtual void printGem(int,int);
-        virtual void printShrink1(int,int);
-        virtual void printShrink2(int,int);
+        virtual void printGem(int,int);     //Always overwriten
+        virtual void printShrink1(int,int); //Always overwriten
+        virtual void printShrink2(int,int); //Always overwriten
 
         int getColor();
         int getScore();
         char getType();
         bool getFalling();
         void setFalling(bool);
-        bool getIfNew();
-        void setOld();
+        bool getIfNew();        //returns value of newGem
+        void setOld();          //sets newGem to false
     protected:
         int color;
         int score;
-        char type;
+        char type;              //'B' for basic 'X' for undeclared
 };
 
 #endif // GEM_H
